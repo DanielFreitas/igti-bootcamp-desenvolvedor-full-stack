6f928311a604fc196ad5e8946c8097d3fc23fa19
DanielFreitas <daniel.freitas.jnr@gmail.com>
2020-08-30 23:11:48 -0300

    Trabalho Prático do Módulo 4

---
+++ cap04/trabalho-pratico/my-bank-api/models/account/account.schema.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const schema = mongoose.Schema({
+  agencia: {
+    type: Number,
+    required: true,
+  },
+  conta: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+});
+
+const accountsModel = mongoose.model("account", schema, "account");
+
+export { accountsModel };
---
+++ cap04/trabalho-pratico/my-bank-api/models/index.js
@@ -0,0 +1,5 @@
+import db from "../config/db.config.js";
+
+db.account = require("./account/account.schema.js");
+
+export default db;
